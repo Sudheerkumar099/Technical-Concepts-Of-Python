414524c6be0f2a3c0bea1c052132eca1d3dd730d
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-22 16:18:21 +0530

    practice: [Sudheer] Practiced conditional statements

---
+++ conditional_statements.py
@@ -0,0 +1,9 @@
+temperature = 24
+if temperature >30 :
+    print("It's a hot day \ntake some water")
+elif temperature >20:
+    print("It's cold day")
+else:
+    print("bye")
+print("done")
+    
---
+++ operator.py
@@ -0,0 +1,14 @@
+
+print(10>10)
+print(10>=10)
+print(10<10)    
+print(10<=10)
+print(10==10)
+print(10!=10)
+print(True and False)
+print(True or False)
+print(not True)
+print(not False)
+print(ord('A')) #gives the ASCII value of the character
+print(ord('a')) #gives the ASCII value of the character
+print(chr(65)) #gives the character for the ASCII value
