fbe032dd2ee0452df08a81d6aac0d352c36f7423
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-23 10:44:24 +0530

    Practiced the yield and next in the generator

--- lists/generator.py
+++ lists/generator.py
@@ -1,10 +1,24 @@
-def my_generator():
-    yield 1
-    print("this is a generator")
-    yield 2
-    yield 3
-    yield 4
-    return 5
+# def my_generator():
+#     yield 1
+#     print("this is a generator")
+#     yield 2
+#     yield 3
+#     yield 4
 
-for i in my_generator():
-    print(i)
+# for i in my_generator():
+#     print(i)
+    
+def simple_gen():
+  yield "Emil"
+  yield "Tobias"
+  yield "Linus"
+
+gen = simple_gen()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+for  i in gen:
+    print(next(gen)) #this will not work next is used for manual traversing
+# for  i in gen:
+#     print(i)
